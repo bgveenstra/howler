23a4430c81477a0cf92d98a748cd7f64914a7c25
bgveenstra <b.g.veenstra@gmail.com>
2017-05-29 11:04:59 -0700

    add debug; refactor to send env variable to slacker

--- howler.go
+++ howler.go
@@ -10,9 +10,9 @@ import (
 )
 
 
-// handles the "howler" command
+// the "howler" command
 func main() {
-	// @TODO - add better help/usage information (--help flag?)
+	// @TODO - add better CLI 
 	numArgs := len(os.Args)
 	if numArgs < 2 {
 		log.Fatal("Error - Too Few Arguments: howler requires a directory or file name\n usage: howler /tmp")
@@ -25,17 +25,24 @@ func main() {
 }
 
 
-func WatchDirForever(dir string) error {
+var verbose = false
+func debugLog(label string, message string){
+	if verbose {
+		log.Printf("%s: %s", label, message)
+	}
+}
 
+// wrap slacker.PostSlackMessage, passing environment variable
+func slack(message string) error{
+	return slacker.PostSlackMessage(message, os.Getenv("HOWLER_SLACK_WEBHOOK_URL"))
+}
+
+func WatchDirForever(dir string) error {
 	// @TODO watch types should come from flag or config - some arg
 	// @TODO configure messages elsewhere for organization?
 	eventWatchTypes := make(map[string] string)
 	eventWatchTypes["CREATE"] = "created"
 
-	watchDir, err := filepath.Abs(dir)
-	if err != nil {
-		return err
-	}
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
 		return err
@@ -47,15 +54,20 @@ func WatchDirForever(dir string) error {
 		for {
 			select {
 			case event := <-watcher.Events:
-				// @TODO - enable logging if "verbose" mode
-				// log.Println("event:", event)
+				debugLog("event", event.String())
+
 				// @TODO - move all or part of message generation to helper function
 				verb, isWatchType := eventWatchTypes[event.Op.String()]
 				if isWatchType {
-					slacker.PostSlackMessage(fmt.Sprintf("File %s: %s\n", verb, event.Name))
+					message := fmt.Sprintf("File %s: %s\n", verb, event.Name)
+					debugLog("message", message)
+					err := slack(message)
+					if err != nil {
+						log.Fatal(err)
+					}
 				}
 			case err := <-watcher.Errors:
-				log.Println("error:", err)
+				log.Fatal(err)
 			}
 		}
 	}()
@@ -63,9 +75,16 @@ func WatchDirForever(dir string) error {
 	err = watcher.Add(dir)
 	if err != nil {
 		return err
-	log.Println("Watching", watchDir)
-	// @TODO - also move this message generation elsewhere
-	slacker.PostSlackMessage(fmt.Sprintf("Watching %s", watchDir))
+	}
+
+	// @TODO - can move this message generation elsewhere
+	watchDir, err := filepath.Abs(dir)
+	if err != nil {
+		return err
+	}
+	message := fmt.Sprintf("Watching %s", watchDir)
+	debugLog("watch message", message)
+	slack(message)
 
 	// @TODO - replace with waitgroup
 	done := make(chan bool)
